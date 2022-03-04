bce0cae933a4f93977c299cecdcfa9f4733e5da4
rynxiao <yuzhongzi91@sina.com>
2022-03-04 17:53:22 +0800

    feat: add fonts

--- app/src/main/java/com/tutorials/mypets/ui/theme/Type.kt
+++ app/src/main/java/com/tutorials/mypets/ui/theme/Type.kt
@@ -2,9 +2,19 @@ package com.tutorials.mypets.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.tutorials.mypets.R
+
+val fonts = FontFamily(
+    Font(R.font.fredoka_regular),
+    Font(R.font.fredoka_bold, weight = FontWeight.Bold),
+    Font(R.font.fredoka_light, weight = FontWeight.Light),
+    Font(R.font.fredoka_medium, weight = FontWeight.Medium),
+    Font(R.font.fredoka_semi_bold, weight = FontWeight.SemiBold)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
